2a0e3523e5b71a10db1655af7d76a66462602148
efmanu <manuedavakandam@gmail.com>
2021-01-07 00:02:36 +1100

    subspace_construction

--- src/SubspaceInference.jl
+++ src/SubspaceInference.jl
@@ -8,14 +8,15 @@ using Flux: @epochs
 
 using LinearAlgebra
 using TSVD
+using Turing
 
 ###########
 # Exports #
 ###########
-export  subspace
+export  subspace_construction
 
-function extract_weights(model::Chain{Tuple{Flux.Dense{typeof(identity),Array{T,2},Array{T,1}}}}, shape) where {T}
-	all_W = Array{T}(undef,0)
+function extract_weights(model, shape)
+	all_W = Array{Float64}(undef,0)
 	for i in 1:length(shape)
 		append!(all_W, model[i].W)
 		append!(all_W, model[i].b)
@@ -24,7 +25,7 @@ function extract_weights(model::Chain{Tuple{Flux.Dense{typeof(identity),Array{T,
 end
 
 """
-    subspace(model, shape, cost, data, opt, callback;T = 10, η = 0.1, c = 1, M = 2, svd_len = 1)
+    subspace_construction(model, shape, cost, data, opt, callback;T = 10, η = 0.1, c = 1, M = 2, svd_len = 1)
 Izmailov, P., Maddox, W. J., Kirichenko, P., Garipov, T., Vetrov, D., & Wilson, A. G. (2020, August). Subspace inference for Bayesian deep learning. 
 In Uncertainty in Artificial Intelligence (pp. 1169-1179). PMLR.
 
@@ -50,7 +51,7 @@ To construct subspace from pretrained weights.
 
 
 """
-function subspace(model, shape, cost, data, opt, callback;T = 10, c = 1, M = 2, svd_len = 1)
+function subspace_construction(model, shape, cost, data, opt, callback;T = 10, c = 1, M = 2, svd_len = 1)
 	m_swa = model #mean model
 
 	W_swa = extract_weights(m_swa, shape);
@@ -81,4 +82,50 @@ function subspace(model, shape, cost, data, opt, callback;T = 10, c = 1, M = 2, 
 	return W_swa, P
 end
 
+function subspace_inference(data, model)
+	#create subspace P
+	args = (model, shape, cost, data, opt, callback, T, c, M, svd_len)
+	w_cap, P = subspace_construction(args...)
+	@model function infer(w_cap, P, model_shape, in_data, out_data)
+		#prior Z
+		z ~ Normal(0.0,1.0)
+		w_til = w_cap + P*z
+
+		pred = ai_model(w_til, data, model_shape, in_data)
+
+		for n in 1:length(out_data)
+		    # Heads or tails of a coin are drawn from a Bernoulli distribution.
+		    pred[i] ~ Normal(pred[i], 0.1)
+		end
+	end
+
+	model = infer(w_cap, P)
+	chn = sample(model, NUTS(0.65), iterations)
+
+end
+
+function generate_model(w_til, shape)
+	layers = Array{Any,1}(undef,0)
+	for i in 1:length(shape)
+		current_len = sum(shape[i][1])+sum(shape[i][2])
+		prev_len = 1
+		if i != 1
+			prev_len = sum(shape[i-1][1])+sum(shape[i-1][2])
+		w = w_til(prev_len:current_len)
+
+		w = w_til(shape[i][sum(shape[i][1])])
+		append!(all_W, model[i].W)
+		append!(all_W, model[i].b)
+		append!(layers, [Dense(rand(2,2),rand(2))])
+
+	end
+	flux_model(x) = foldl((x,m) -> m(x), layers, init =x)
+
+	return flux_model
+end
+
+function ai_model(w_til, data, model_shape, in_data)
+
+	return pred
+end
 end # module
